6de4c88753f35353137daaebbeafddbaa0dc7703
Darek Sikorra <dariusz.sikorra@gmail.com>
2019-04-30 11:01:40 +0200

    First commit

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt in id
+      vel omnis. Dolores ullam impedit porro doloribus esse numquam recusandae
+      delectus vitae quam praesentium, tempora libero asperiores ut dolore.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam rerum dolor
+      dicta illo incidunt accusamus ad, quaerat beatae aspernatur. Nobis
+      inventore recusandae voluptates nulla laborum dignissimos vitae ipsam
+      autem aliquid!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+      odio a tempore repellat aperiam fuga veritatis voluptate ipsam veniam
+      minus beatae cum, porro alias, iusto corporis. Ratione nostrum velit
+      itaque!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam rerum dolor
+      dicta illo incidunt accusamus ad, quaerat beatae aspernatur. Nobis
+      inventore recusandae voluptates nulla laborum dignissimos vitae ipsam
+      autem aliquid!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+      odio a tempore repellat aperiam fuga veritatis voluptate ipsam veniam
+      minus beatae cum, porro alias, iusto corporis. Ratione nostrum velit
+      itaque!
+    </p>
+  </div>
+);
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt in id
+      vel omnis. Dolores ullam impedit porro doloribus esse numquam recusandae
+      delectus vitae quam praesentium, tempora libero asperiores ut dolore.
+    </p>
+  </div>
+);
